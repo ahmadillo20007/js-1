655bc787ed3d8303b1f2ca4c546f6c394e63933c
Ahmadillo20007 <ahmadilloqubijonov@gmail.com>
2023-02-12 23:00:08 +0500

    ghj

---
+++ js/data.js
@@ -0,0 +1,45 @@
+const data = {
+    nav:[
+    {
+        id:1,
+        titleUZ:'Kompaniya haqida',
+        titleEN:'About the company',
+        titleRU:'О компании'
+    },    {
+        id:2,
+        titleUZ:'faktlar',
+        titleEN:'the facts',
+        titleRU:'факты'
+    },{
+        id:3,
+        titleUZ:'mahsulot',
+        titleEN:'product',
+        titleRU:'продукт'
+    },{
+        id:4,
+        titleUZ:'buyurtma berish',
+        titleEN:'placing an order',
+        titleRU:'размещение заказа'
+    }
+    ],
+    hero:[
+{
+    id:1,
+    titleUZ:'100% tabiiy',
+    titleEN:'100% natural',
+    titleRU:'100% натуральный'
+},{
+    id:2,
+    titleUZ:'Koreya qizil jenshenichimligi',
+    titleEN:'Korean red ginseng',
+    titleRU:'Корейский красный женьшень'
+},{
+    id:3,
+    titleUZ:'BUYURTMA BERISH',
+    titleEN:'PLACE AN ORDER',
+    titleRU:'ОФОРМИТЬ ЗАКАЗ'
+}
+    ]
+    
+}
+
---
+++ js/main.js
@@ -0,0 +1,54 @@
+let elNavList = document.querySelector('.nav__list');
+let elHeroInner = document.querySelector('.hero__inner');
+let elLengSel = document.querySelector('.lang__select')
+let elLangOpt = document.querySelectorAll('.len__opt')
+
+console.log(data.hero);
+
+function etitLangeage(leng){
+    window.localStorage.setItem('long', leng)
+    elHeroInner.innerHTML = ''
+    elNavList.innerHTML = ''
+
+    data.nav.map((item) => {
+       
+        let newLI = document.createElement('li')
+        newLI.textContent = item[`title${leng}`]
+        elNavList.appendChild(newLI)
+      })
+      
+      let newP = document.createElement('b');
+      newP.textContent = data.hero[0][`title${leng}`]
+      elHeroInner.appendChild(newP);
+      
+      let newH2 = document.createElement('h2');
+      newH2.textContent = data.hero[1][`title${leng}`]
+      elHeroInner.appendChild(newH2);
+      let newA = document.createElement('a');
+      newA.textContent = data.hero[2][`title${leng}`]
+      newA.href = '#'
+      elHeroInner.appendChild(newA);
+}
+let logalLong = window.localStorage.getItem('long')
+if(logalLong){
+    etitLangeage(logalLong )
+}else{
+    etitLangeage('UZ')
+}
+
+elLengSel.addEventListener('change',(e)=>{
+
+    etitLangeage(e.target.value)
+
+})
+elLangOpt.forEach((item)=>{
+   (item.value);
+if(logalLong == item.value){
+  item.setAttribute('selected', true)
+}
+})
+
+
+
+
+
